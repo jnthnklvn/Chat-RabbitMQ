e8da57a7f0028ec950ab6450c8bbee3e395ab11c
jnthnklvn <jonathan.santos@dcomp.ufs.br>
2019-02-15 23:00:52 +0000

    commit

--- src/main/java/br/ufs/dcomp/ChatRabbitMQ/Chat.java
+++ src/main/java/br/ufs/dcomp/ChatRabbitMQ/Chat.java
@@ -1,20 +1,27 @@
 package br.ufs.dcomp.ChatRabbitMQ;
 
 import com.rabbitmq.client.*;
-
+import java.util.Scanner;
 import java.io.IOException;
 
 public class Chat {
 
   public static void main(String[] argv) throws Exception {
     ConnectionFactory factory = new ConnectionFactory();
-    factory.setHost("ip-da-instancia-da-aws"); // Alterar
-    factory.setUsername("usuário-do-rabbitmq-server"); // Alterar
-    factory.setPassword("senha-do-rabbitmq-server"); // Alterar
+    factory.setHost("ec2-3-88-85-161.compute-1.amazonaws.com"); // Alterar
+    factory.setUsername("zkelvinfps"); // Alterar
+    factory.setPassword("0"); // Alterar
     factory.setVirtualHost("/");
     Connection connection = factory.newConnection();
     Channel channel = connection.createChannel();
-    
+    Scanner sc = new Scanner(System.in);
+    System.out.print("User: ");
+    String usuario = sc.next();
+    System.out.print(">> ");
+    while(true){
+      String destinatario = sc.next();
+      if ()
+    }
     String QUEUE_NAME = "minha-fila";
                       //(queue-name, durable, exclusive, auto-delete, params); 
     channel.queueDeclare(QUEUE_NAME, false,   false,     false,       null);
