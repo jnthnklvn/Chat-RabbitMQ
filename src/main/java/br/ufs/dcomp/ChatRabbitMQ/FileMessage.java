b01bf743a0c5687deb3dcfd78a831b36ff2068b7
Jonathan Kelvin <j_kelvin1997@hotmail.com>
2019-03-30 01:25:29 -0300

    Downloads path att

--- src/main/java/br/ufs/dcomp/ChatRabbitMQ/FileMessage.java
+++ src/main/java/br/ufs/dcomp/ChatRabbitMQ/FileMessage.java
@@ -6,8 +6,9 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /**
- * Classe responsável pelo tratamento de arquivos de mensagem.
- * Transformação e download de arquivos de arquivo em bytes.
+ * Classe responsável pelo tratamento de arquivos de mensagem. Transformação e
+ * download de arquivos de arquivo em bytes.
+ * 
  * @version 1.0
  * @since Finalização da Etapa 3
  */
@@ -15,7 +16,9 @@ public class FileMessage {
     private String filePath;
 
     /**
-     * Método construtor, responsável por inicializar a váriavel de caminho para o arquivo.
+     * Método construtor, responsável por inicializar a váriavel de caminho para o
+     * arquivo.
+     * 
      * @param filePath - String com endereço de um arquivo ou diretório.
      */
     public FileMessage(String filePath) {
@@ -24,6 +27,7 @@ public class FileMessage {
 
     /**
      * Encontra o tipo de um arquivo e o retorna numa String.
+     * 
      * @return tipoMime - String com o tipo do arquivo.
      */
     public String getTipo() {
@@ -38,21 +42,24 @@ public class FileMessage {
     }
 
     /**
-     * Transforma uma cópia od arquivo, a partir de seu endereço, em um Array de bytes.
+     * Transforma uma cópia od arquivo, a partir de seu endereço, em um Array de
+     * bytes.
+     * 
      * @return dataAsByte - Array de bytes com representação do arquivo.
      */
     public byte[] getFileBytes() {
         byte[] dataAsByte = null;
         File file = new File(filePath);
-        
+
         try {
             FileInputStream inputstream = new FileInputStream(this.filePath);
-            dataAsByte = new byte[(int)file.length()];
+            dataAsByte = new byte[(int) file.length()];
 
-            while(inputstream.read(dataAsByte) != -1);
-            
+            while (inputstream.read(dataAsByte) != -1)
+                ;
+
             inputstream.close();
-            
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -61,14 +68,22 @@ public class FileMessage {
 
     /**
      * Escreve um arquivo a partir de um Array de bytes em determinado local.
-     * @param buf - Array de bytes com os bytes do arquivo.
+     * 
+     * @param buf      - Array de bytes com os bytes do arquivo.
      * @param fileName - String com o nome do arquivo (incluindo o caminho).
      */
     public void downloadFile(byte[] buf, String fileName) {
+        Path path = Paths.get("downloads/");
+        
         FileOutputStream fos = null;
 
         try {
-            fos = new FileOutputStream(this.filePath + "/" + fileName);
+            if (!Files.exists(path)) {
+                Files.createDirectory(path);
+            }
+            path = Paths.get("downloads/" + fileName);
+
+            fos = new FileOutputStream(path.toString());
             fos.write(buf, 0, buf.length);
             fos.flush();
             fos.close();
